c70b6b0ccd40874a41d18fc5bf11285a425d81ee
leandre <tuyambazeleandre@gmail.com>
2024-03-22 04:08:57 +0200

    Return customised error to the client side

--- src/main/java/com/allycode/Main.java
+++ src/main/java/com/allycode/Main.java
@@ -2,6 +2,7 @@ package com.allycode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 
@@ -9,9 +10,17 @@ public class Main {
 
 
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        ConfigurableApplicationContext
+                applicationContext = SpringApplication.run(Main.class, args);
+
+
+
+}
+
+    private static void printBeans(ConfigurableApplicationContext ctx) {
+        String[] beanDefinitionNames = ctx.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            System.out.println(beanDefinitionName);
+        }
     }
-
-
-
 }
